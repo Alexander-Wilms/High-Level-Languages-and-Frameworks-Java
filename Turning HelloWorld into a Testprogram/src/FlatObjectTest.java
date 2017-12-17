92a4d1b3660e4744a64b52ea154bcef4fcf5809f
Alexander Wilms <f.alexander.wilms@gmail.com>
2017-12-17 03:15:08 +0100

    Small fix in Turning HelloWorld into a Testprogram

--- Turning HelloWorld into a Testprogram/src/FlatObjectTest.java
+++ Turning HelloWorld into a Testprogram/src/FlatObjectTest.java
@@ -2,7 +2,7 @@ public class FlatObjectTest {
     static FlatObject myFlat;
 
     public static void main(String[] args) {
-        System.out.println("FlatObjectTest");
+        System.out.println("FlatObjectTest!");
         // if we don't actually create the object, we get NullPointerExceptions later on
         myFlat = new FlatObject();
         myFlat.setArea(3.5);
