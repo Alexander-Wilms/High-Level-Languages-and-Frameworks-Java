693d4da95fd4ebe2a6c481262ff04233c3464866
Alexander Wilms <f.alexander.wilms@gmail.com>
2017-12-17 01:38:49 +0100

    Java Standalone Program

---
+++ Java Standalone Program/src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // System is the class
+        // out is the field
+        // println is the method
+        System.out.println("Hello World!");
+    }
+}
